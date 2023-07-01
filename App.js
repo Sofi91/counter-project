cf2a072f7f2474ab42eadceec53efac8e2ae0dfc
Sofi91 <113124441+Sofi91@users.noreply.github.com>
2023-07-01 16:13:22 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import ClassComponents from "./components/ClassComponents";
+import  './styles/App.css';
+
+
+
+function App() {
+    return (
+        <div className="App">
+        <ClassComponents />
+        </div>
+    )
+}
+export default App;
